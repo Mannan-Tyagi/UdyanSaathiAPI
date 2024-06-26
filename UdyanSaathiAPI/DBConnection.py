fd2a1c949db54e6f6153d5fb4c31996027c204b1
Mannan <Mannan@DESKTOP-OTIQOF5>
2024-06-27 02:25:52 +0530

    updated

--- UdyanSaathiAPI/DBConnection.py
+++ UdyanSaathiAPI/DBConnection.py
@@ -1,10 +1,11 @@
 import mysql.connector
-
+import os
 
 # class DBConnection:
 #     
 class DBConnection:
     # keyword = "Azure"
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     keyword = "Azure"
     if(keyword == "Azure"):
         @classmethod
@@ -15,7 +16,7 @@ class DBConnection:
                 'password': 'Khetan@123',
                 'database': 'udyaansaathidata',
                 'client_flags': [mysql.connector.ClientFlag.SSL],
-                'ssl_ca': '/var/www/html/DigiCertGlobalRootG2.crt.pem'
+                'ssl_ca': os.path.join(BASE_DIR, 'certificates', 'DigiCertGlobalRootG2.crt.pem')
 
             }
 
